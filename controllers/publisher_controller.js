0bc9b5f3e82c88a64028782bc1e1c18fb18b3647
Chathu Abeywickrama <chathu.git93@gmail.com>
2018-09-21 10:15:29 +0530

    Publisher Controller Added

---
+++ controllers/publisher_controller.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Publisher = require('../models/publisher_model');
+
+/**
+ * GET this will return all the Publisher
+ */
+exports.get_all_genres =(req,res,next)=>{
+    Publisher.find().select("name").exec()
+        .then(docs =>{
+            const response = {
+                count: docs.length,
+                Publisher: docs.map(doc =>{
+                    return{
+                        _id: doc._id,
+                        name: doc.name
+                    }
+                })
+            }
+            console.log(docs);
+            res.status(200).json(docs);
+        })
+        .catch(err =>{
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        })
+}
+
+/**
+ * POST this will post a new iTem to Publishers
+ */
+exports.add_genre = (req, res, next)=>{
+    const genre = new Publisher({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+    })
+    genre.save().then(result =>{
+        console.log(result);
+        res.status(200).json({
+            message: "Publisher has been added",
+            publisherDetails: result
+        });
+    })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        })
+}
